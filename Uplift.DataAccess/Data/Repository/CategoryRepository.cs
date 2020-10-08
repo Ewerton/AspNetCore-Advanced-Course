146a1fb673e455edf25a219c9c81bb4ddbb4b10b
Ewerton Mattos <ewerton.sc@gmail.com>
2020-10-07 22:08:18 -0300

    Implementado o repositorio de Categoria

---
+++ Uplift.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetCategoryListForDropDown()
+        {
+            return _db.Category.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDB = _db.Category.FirstOrDefault(s => s.Id == category.Id);
+
+            ///TODO: Tem como automatizar?
+            objFromDB.Name = category.Name;
+            objFromDB.DisplayOrder = category.DisplayOrder;
+
+            _db.SaveChanges();
+        }
+    }
+}
