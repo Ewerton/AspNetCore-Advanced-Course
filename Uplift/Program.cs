1026f99aec06665158ae944c5bfd883650d8369a
Ewerton Mattos <ewerton.sc@gmail.com>
2021-03-01 21:30:27 -0300

    Revert "checking if the app is running on azure or locally"
    This reverts commit 9f3fb746106fec852e75cfa8cc03029227fe8f0e.

--- Uplift/Program.cs
+++ Uplift/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Azure.Identity;
 
 namespace Uplift
 {
@@ -17,32 +16,11 @@ namespace Uplift
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            // Se estiver rodando no Azure, vai ter a VaultUri
-            var isRunningOnAzure = !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"));
-            if (isRunningOnAzure)
-            {
-                string vaultURI = Environment.GetEnvironmentVariable("VaultUri");
-                if (!String.IsNullOrWhiteSpace(vaultURI))
-                {
-                    var keyVaultEndpoint = new Uri(vaultURI);
-
-                    Host.CreateDefaultBuilder(args)
-                        .ConfigureAppConfiguration((context, config) =>
-                        {
-
-                            config.AddAzureKeyVault(
-                            keyVaultEndpoint,
-                            new DefaultAzureCredential());
-                        });
-                }
-            }
-
-            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-        }
     }
 }
